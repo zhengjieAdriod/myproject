86789fa179dbb14467370a2ca90e69e5cda7f4c5
zhangxiaoqi <zhangxiaoqi@ichinait.com>
2017-08-23 18:26:04 +0800

    form表单,同一个视图函数

--- renovateproject/worker_manage/forms.py
+++ renovateproject/worker_manage/forms.py
@@ -7,3 +7,8 @@ class ServiceForm(forms.ModelForm):
     class Meta:
         model = Service
         fields = ['image', 'worker', 'name', 'price', 'describe', 'scope', 'type']
+
+
+class AddForm(forms.Form):
+    a = forms.IntegerField()
+    b = forms.IntegerField()
--- renovateproject/worker_manage/urls.py
+++ renovateproject/worker_manage/urls.py
@@ -9,9 +9,10 @@ app_name = 'worker_manage'  # 告诉 django 这个 urls.py 模块是属于 blog 
 # 下面的是rest framwork 的路由
 router = routers.DefaultRouter()
 urlpatterns = [
+    url(r'^form/$', views.index, name='form-url'),
     url(r'^manager/$', views.manager, name='manager-url'),
     url(r'^tab_manage/$', views.tab_manage, name='tab_manage-url'),
-    url(r'^edit_service/(?P<service_pk>[0-9]+)/$', views.edit_service, name='edit_service-url'),
+    url(r'^edit_service/(?P<service_pk>[0-9]+)/$', views.update_edit_service, name='edit_service-url'),
     url(r'^update_edit_service/(?P<service_pk>[0-9]+)/$', views.update_edit_service, name='update_edit_service-url'),
     # 下面的是rest framwork 的路由update_edit_service
     url(r'^', include(router.urls)),
