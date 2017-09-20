f5a4fa83e89b671debfb6bebcdaaee18195d3248
zhangxiaoqi <zhangxiaoqi@ichinait.com>
2017-09-20 18:28:57 +0800

    该做添加空服务子项

--- renovateproject/worker_manage/urls.py
+++ renovateproject/worker_manage/urls.py
@@ -12,12 +12,20 @@ urlpatterns = [
     url(r'^form/$', views.index, name='form-url'),
     url(r'^manager/(?P<worker_pk>[0-9]+)/$', views.manager, name='manager-url'),
     url(r'^tab_manage/$', views.tab_manage, name='tab_manage-url'),
-    url(r'^edit_service/(?P<service_pk>[0-9]+)/$', views.update_edit_service, name='edit_service-url'),
-    url(r'^add_service/(?P<worker_pk>[0-9]+)/$', views.add_service, name='add_service-url'),
-    url(r'^post_service_item/(?P<worker_pk>[0-9]+)/(?P<service_pk>[0-9]+)/(?P<form_item_pk>[0-9]+)/$', views.post_service_item,
-        name='post_service_item-url'),
 
-    # url(r'^update_edit_service/(?P<service_pk>[0-9]+)/$', views.update_edit_service, name='update_edit_service-url'),
+    # todo 点击条增加服务按钮, 以及保存首次新增的服务
+    url(r'^add_service/(?P<worker_pk>[0-9]+)/$', views.add_service, name='add_service-url'),
+
+    # todo 点击条目的编辑按钮, 以及保存编辑后的服务和服务子项
+    url(r'^edit_service/(?P<service_pk>[0-9]+)/$', views.update_edit_service, name='edit_service-url'),
+
+    # todo 点击添加子项按钮
+    url(r'^add_item_service/(?P<service_pk>[0-9]+)/$', views.add_item_service, name='add_item_service-url'),
+    # todo 单个服务子项的保存
+    # url(r'^post_service_item/(?P<worker_pk>[0-9]+)/(?P<service_pk>[0-9]+)/(?P<form_item_pk>[0-9]+)/$',
+    #     views.post_service_item,
+    #     name='post_service_item-url'),
+
     # 下面的是rest framwork 的路由update_edit_service
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
--- renovateproject/worker_manage/views.py
+++ renovateproject/worker_manage/views.py
@@ -42,7 +42,7 @@ def tab_manage(request):
     return render(request, 'worker_manage/tab_errer.html', context={'test': '访问失败'})
 
 
-# 增加service 并关联worker
+# todo 增加service 并关联worker
 def add_service(request, worker_pk):
     workers = Worker.objects.filter(pk=worker_pk)
     if len(workers) > 0:
@@ -75,86 +75,6 @@ def add_service(request, worker_pk):
         return render(request, 'worker_manage/add_service.html', context=context)
 
 
-# 增加service 并关联worker
-# def add_service(request, worker_pk):
-#     workers = Worker.objects.filter(pk=worker_pk)
-#     if len(workers) > 0:
-#         worker = workers[0]
-#         worker_pk = worker.pk
-#         if request.method == 'POST':
-#             form = ServiceForm(request.POST)
-#             if form.is_valid():
-#                 service_form = form.save(commit=False)
-#
-#                 file_dic = request.FILES.dict()
-#                 l = len(file_dic)
-#                 if l > 0:
-#                     image = file_dic['image']
-#                     service_form.image = image
-#                 # 将评论和被评论的文章关联起来。
-#                 service_form.worker = worker
-#                 service_form.save()
-#                 # return redirect(worker)  # todo 第一种重定向方式
-#                 # todo 第二种重定向方式
-#                 # return redirect(reverse('worker_manage:manager-url', kwargs={'worker_pk': worker_pk}))
-#                 form_item_list = []
-#                 form_item = SchemeInServiceForm()
-#                 form_item_list.append(form_item)
-#                 form_item_list.append(form_item)
-#                 context = {'service_pk': service_form.pk, 'worker_pk': worker.pk, 'form': form,
-#                            'form_item_list': form_item_list}
-#                 return render(request, 'worker_manage/add_service.html', context=context)
-#             else:
-#                 # 检查到数据不合法，重新渲染详情页，并且渲染表单的错误。
-#                 context = {'form': form}
-#                 return render(request, 'worker_manage/add_service.html', context=context)
-#         # get请求时
-#         form = ServiceForm()
-#         form_item_list = []
-#         form_item = SchemeInServiceForm()
-#         form_item_list.append(form_item)
-#         form_item_list.append(form_item)
-#         # -1来区别按钮是否可以点击
-#         context = {'service_pk': -1, 'worker_pk': worker.pk, 'form': form, 'form_item_list': form_item_list}
-#         return render(request, 'worker_manage/add_service.html', context=context)
-
-
-# 增加service的子项 并关联worker，主service
-def post_service_item(request, worker_pk, service_pk, form_item_pk):
-    workers = Worker.objects.filter(pk=worker_pk)
-    if len(workers) > 0:
-        worker = workers[0]
-        worker_pk = worker.pk
-        scheme_in_services = SchemeInService.objects.filter(pk=form_item_pk)
-
-        if request.method == 'POST':
-            if len(scheme_in_services) > 0:
-                scheme_in_service = scheme_in_services[0]
-                form = SchemeInServiceForm(request.POST, instance=scheme_in_service)
-            else:
-                form = SchemeInServiceForm(request.POST)
-            if form.is_valid():
-                scheme_in_service_form = form.save(commit=False)
-
-                file_dic = request.FILES.dict()
-                l = len(file_dic)
-                if l > 0:
-                    image = file_dic['image']
-                    scheme_in_service_form.image = image
-                    # 将评论和被评论的文章关联起来。
-                service = Service.objects.get(pk=service_pk)
-                scheme_in_service_form.service = service
-                scheme_in_service_form.save()
-                service.schemeinservice_set.add(scheme_in_service_form)
-                # return redirect(worker)  # todo 第一种重定向方式
-                # todo 第二种重定向方式
-                return redirect(reverse('worker_manage:manager-url', kwargs={'worker_pk': worker_pk}))
-            else:
-                # 检查到数据不合法，重新渲染详情页，并且渲染表单的错误。
-                context = {'form': form}
-                return render(request, 'worker_manage/edit_service.html', context=context)
-
-
 def all_valid(list):
     for item in list:
         if not item.is_valid():
@@ -166,23 +86,30 @@ def all_valid(list):
 # @api_view(['POST'])
 # @csrf_exempt
 # @permission_classes((AllowAny,))
+# todo 点击条目的编辑按钮展示服务数据包括服务的子项数据(同时也是提交编辑后的按钮)
 def update_edit_service(request, service_pk):
     service = get_object_or_404(Service, pk=service_pk)
     worker_pk = service.worker.pk
     scheme_in_service_set = service.schemeinservice_set
+
     scheme_in_service_list = scheme_in_service_set.all()
     if request.method == 'POST':
         # 我们利用这些数据构造了 CommentForm 的实例，这样 Django 的表单就生成了。
         # todo 有了第二个参数 instance = service , 便是对原来数据的更新,而不是添加新的数据
         # form = ServiceForm(request.POST)
-        form = ServiceForm(request.POST, request.FILES, instance=service)
+        form = ServiceForm(request.POST, request.FILES, instance=service, prefix='form')
         list = []
+        item_form = SchemeInServiceForm(request.POST, request.FILES,
+                                        prefix='item_form')
+        list.append(item_form)
+        i = 0
         for scheme_in_service in scheme_in_service_list:
-            item_form = SchemeInServiceForm(request.POST, request.FILES, instance=scheme_in_service)
+            i = i + 1
+            item_form = SchemeInServiceForm(request.POST, request.FILES, instance=scheme_in_service,
+                                            prefix='item_form' + str(i))
             list.append(item_form)
-        if len(scheme_in_service_list)==0:
+        if len(scheme_in_service_list) == 0:
             pass
-
 
         # 当调用 form.is_valid() 方法时，Django 自动帮我们检查表单的数据是否符合格式要求。
         if form.is_valid() and all_valid(list):
@@ -199,25 +126,37 @@ def update_edit_service(request, service_pk):
             # return redirect(worker)  # todo 第一种重定向方式
             # todo 第二种重定向方式
             return redirect(reverse('worker_manage:manager-url', kwargs={'worker_pk': worker_pk}))
+            # form_item = SchemeInServiceForm(prefix='item_form')
+            # list.append(form_item)
+            # context = {'worker_pk': worker_pk, 'service_pk': service_pk, 'form': form,
+            #            'form_item_list': list}
+            # return render(request, 'worker_manage/edit_service.html', context=context)
         else:
             context = {'service_pk': service_pk, 'service': service, 'form': form}
             return render(request, 'worker_manage/edit_service.html', context=context)
+
     form_item_list = []
+
+    form_item = SchemeInServiceForm(prefix='item_form')
+    # todo 判断是否添加空form  否则每次进到该页面都会重复添加空form
+    form_item_list.append(form_item)
+    j = 0
     for scheme_in_service in scheme_in_service_list:
+        j = j + 1
         form_item = SchemeInServiceForm(initial={'pk': scheme_in_service.pk,
                                                  'name': scheme_in_service.name,
                                                  'price': scheme_in_service.price,
                                                  'describe': scheme_in_service.describe,
-                                                 'image': scheme_in_service.image})
+                                                 'image': scheme_in_service.image, 'Service': service},
+                                        prefix='item_form' + str(j))
         form_item.pk = scheme_in_service.pk
         form_item_list.append(form_item)
-    if len(scheme_in_service_list) < 1:
-        form_item = SchemeInServiceForm()
-        form_item.pk = 0
-        form_item_list.append(form_item)
+    # if len(scheme_in_service_list) < 1:
+    #     form_item = SchemeInServiceForm()
+    #     form_item.pk = 0
+    #     form_item_list.append(form_item)
     # 另外多加的一项，隐藏起来
     # form_item = SchemeInServiceForm()
-    # form_item.pk = 0
     # form_item_list.append(form_item)
     form = ServiceForm(initial={'pk': service.pk, 'name': service.name,
                                 'scope': service.scope,
@@ -225,84 +164,90 @@ def update_edit_service(request, service_pk):
                                 'type': service.type,
                                 'describe': service.describe,
                                 'worker': service.worker,
-                                'image': service.image})
+                                'image': service.image}, prefix='form')
     context = {'worker_pk': worker_pk, 'service_pk': service_pk, 'form': form,
                'form_item_list': form_item_list}
     return render(request, 'worker_manage/edit_service.html', context=context)
 
 
-# def update_edit_service(request, service_pk):
-#     service = get_object_or_404(Service, pk=service_pk)
-#     worker_pk = service.worker.pk
-#     if request.method == 'POST':
-#         # 我们利用这些数据构造了 CommentForm 的实例，这样 Django 的表单就生成了。
-#         # todo 有了第二个参数 instance = service , 便是对原来数据的更新,而不是添加新的数据
-#         # form = ServiceForm(request.POST)
-#         form = ServiceForm(request.POST, instance=service)
-#         # 当调用 form.is_valid() 方法时，Django 自动帮我们检查表单的数据是否符合格式要求。
-#         if form.is_valid():
-#             # 检查到数据是合法的，调用表单的 save 方法保存数据到数据库，
-#             # commit=False 的作用是仅仅利用表单的数据生成 Comment 模型类的实例，但还不保存评论数据到数据库。
-#             service_form = form.save(commit=False)
-#             file_dic = request.FILES.dict()
-#             l = len(file_dic)
-#             if l > 0:
-#                 image = file_dic['image']
-#                 service_form.image = image
-#             # 将评论和被评论的文章关联起来。
-#             # comment.post = post
-#             # 最终将评论数据保存进数据库，调用模型实例的 save 方法
-#             service_form.worker = service.worker
-#             service_form.save()
-#             # return redirect(worker)  # todo 第一种重定向方式
-#             # todo 第二种重定向方式
-#             return redirect(reverse('worker_manage:manager-url', kwargs={'worker_pk': worker_pk}))
-#         else:
-#             # 检查到数据不合法，重新渲染详情页，并且渲染表单的错误。
-#             # 因此我们传了三个模板变量给 detail.html，
-#             # 一个是文章（Post），一个是评论列表，一个是表单 form
-#             # 注意这里我们用到了 post.comment_set.all() 方法，
-#             # 这个用法有点类似于 Post.objects.all()
-#             # 其作用是获取这篇 post 下的的全部评论，
-#             # 因为 Post 和 Comment 是 ForeignKey 关联的，
-#             # 因此使用 post.comment_set.all() 反向查询全部评论。
-#             # 具体请看下面的讲解。
-#             context = {'service_pk': service_pk, 'service': service, 'form': form}
-#             return render(request, 'worker_manage/edit_service.html', context=context)
-#             # 不是 post 请求，说明用户没有提交数据，重定向到文章详情页。
-#             # return render(request, 'worker_manage/edit_service.html', context=context)
-#     # service_serializer = ServiceSerializer(service, many=False)
-#     # 'service': json.dumps(service_serializer.data),
-#     # form的初始化
-#     scheme_in_service_set = service.schemeinservice_set
-#     scheme_in_service_list = scheme_in_service_set.all()
-#     form_item_list = []
-#     for scheme_in_service in scheme_in_service_list:
-#         form_item = SchemeInServiceForm(initial={'pk': scheme_in_service.pk,
-#                                                  'name': scheme_in_service.name,
-#                                                  'price': scheme_in_service.price,
-#                                                  'describe': scheme_in_service.describe,
-#                                                  'image': scheme_in_service.image})
-#         form_item.pk = scheme_in_service.pk
-#         form_item_list.append(form_item)
-#     if len(scheme_in_service_list) < 1:
-#         form_item = SchemeInServiceForm()
-#         form_item.pk = 0
-#         # form_item_list.append(form_item)
-#     # 另外多加的一项，隐藏起来
-#     # form_item = SchemeInServiceForm()
-#     # form_item.pk = 0
-#     # form_item_list.append(form_item)
-#     form = ServiceForm(initial={'pk': service.pk, 'name': service.name,
-#                                 'scope': service.scope,
-#                                 'price': service.price,
-#                                 'type': service.type,
-#                                 'describe': service.describe,
-#                                 'worker': service.worker,
-#                                 'image': service.image})
-#     context = {'worker_pk': worker_pk, 'service_pk': service_pk, 'form': form,
-#                'form_item_list': form_item_list}
-#     return render(request, 'worker_manage/edit_service.html', context=context)
+# todo 点击添加子项服务
+def add_item_service(request, service_pk):
+    service = get_object_or_404(Service, pk=service_pk)
+    worker_pk = service.worker.pk
+    scheme_in_service_set = service.schemeinservice_set
+
+    scheme_in_service = SchemeInService()
+    scheme_in_service.save()
+    scheme_in_service_set.add(scheme_in_service)
+    service.save()
+    scheme_in_service_list = scheme_in_service_set.all()
+
+    form_item_list = []
+    j = 0
+    for scheme_in_service in scheme_in_service_list:
+        j = j + 1
+        form_item = SchemeInServiceForm(initial={'pk': scheme_in_service.pk,
+                                                 'name': scheme_in_service.name,
+                                                 'price': scheme_in_service.price,
+                                                 'describe': scheme_in_service.describe,
+                                                 'image': scheme_in_service.image, 'Service': service},
+                                        prefix='item_form' + str(j))
+        form_item.pk = scheme_in_service.pk
+        form_item_list.append(form_item)
+    # if len(scheme_in_service_list) < 1:
+    #     form_item = SchemeInServiceForm()
+    #     form_item.pk = 0
+    #     form_item_list.append(form_item)
+    # 另外多加的一项，隐藏起来
+    # form_item = SchemeInServiceForm()
+    # form_item_list.append(form_item)
+    form = ServiceForm(initial={'pk': service.pk, 'name': service.name,
+                                'scope': service.scope,
+                                'price': service.price,
+                                'type': service.type,
+                                'describe': service.describe,
+                                'worker': service.worker,
+                                'image': service.image}, prefix='form')
+    context = {'worker_pk': worker_pk, 'service_pk': service_pk, 'form': form,
+               'form_item_list': form_item_list}
+    return render(request, 'worker_manage/edit_service.html', context=context)
+
+
+# # todo 保存单个服务子项
+# def post_service_item(request, worker_pk, service_pk, form_item_pk):
+#     workers = Worker.objects.filter(pk=worker_pk)
+#     if len(workers) > 0:
+#         worker = workers[0]
+#         worker_pk = worker.pk
+#         scheme_in_services = SchemeInService.objects.filter(pk=form_item_pk)
+#
+#         if request.method == 'POST':
+#             if len(scheme_in_services) > 0:
+#                 scheme_in_service = scheme_in_services[0]
+#                 form = SchemeInServiceForm(request.POST, instance=scheme_in_service)
+#             else:
+#                 form = SchemeInServiceForm(request.POST)
+#             if form.is_valid():
+#                 scheme_in_service_form = form.save(commit=False)
+#
+#                 file_dic = request.FILES.dict()
+#                 l = len(file_dic)
+#                 if l > 0:
+#                     image = file_dic['image']
+#                     scheme_in_service_form.image = image
+#                     # 将评论和被评论的文章关联起来。
+#                 service = Service.objects.get(pk=service_pk)
+#                 scheme_in_service_form.service = service
+#                 scheme_in_service_form.save()
+#                 service.schemeinservice_set.add(scheme_in_service_form)
+#                 # return redirect(worker)  # todo 第一种重定向方式
+#                 # todo 第二种重定向方式
+#                 return redirect(reverse('worker_manage:manager-url', kwargs={'worker_pk': worker_pk}))
+#             else:
+#                 # 检查到数据不合法，重新渲染详情页，并且渲染表单的错误。
+#                 context = {'form': form}
+#                 return render(request, 'worker_manage/edit_service.html', context=context)
+
 
 def index(request):
     if request.method == 'POST':  # 当提交表单时
